63168e74dc48e7959c1e21cbcf9fa7f8247be325
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-05-13 19:41:22 +0700

    Melengkapi class buku

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -4,10 +4,11 @@ using namespace std;
 class buku{
     string judul;
     public:
-        buku setJudul(string Judul){
-
+        buku setJudul(string judul){
+            this->judul=judul;
+            return *this; //chain function
         }
-
+        
     string getJudul(){
         return this->judul;
     }
