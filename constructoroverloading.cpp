5e3afa08299e4dfa9266fa995946d969c5f9e1b9
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-05-13 19:17:06 +0700

    Membuat prosedur mahasiswa dan program utama

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -29,3 +29,21 @@ mahasiswa ::mahasiswa(int iNim, string iNama){
     nim = iNim; //definisi hanya NIM dan NAMA
     nama = iNama; 
 }
+
+void mahasiswa ::cetak(){
+    cout <<endl<< "Nim ="<<nim<<endl;
+    cout <<"Nama :"<<nama<<endl;
+}
+
+int main(){
+    mahasiswa mhs1(102030);                  //hanya NIM
+    mahasiswa mhs2("Andi");                  //hanya nama
+    mahasiswa mhs3(010203, "Santi");         //NIM dan NAMA
+
+    //tampilkan nilai
+    cout<<"Mahasiswa 1 :"; mhs1.cetak();
+    cout<<"Mahasiswa 2 :"; mhs2.cetak();
+    cout<<"Mahasiswa 3 :"; mhs3.cetak();
+
+    return 0;
+}
