f5995dae94f16c8318af6ac0446ba94e2faef745
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-05-20 19:10:35 +0700

    Membuat Program Informasi Barang

--- file1.cpp
+++ file1.cpp
@@ -1,2 +1,27 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+class Barang {
+    public:
+    string namaBarang;
+    int kodeBarang;
+    
+    Barang(string namaBarang, int kodeBarang);
+};
+
+Barang :: Barang (string namaBarang, int kodeBarang){
+    cout<<"Nama Barang ="<< namaBarang<<endl;
+    cout<<"Kode Barang ="<< kodeBarang<<endl;
+
+}
+
+int main(){
+    Barang brg("Buku", 1234);
+    return 0;
+}
+
+
+//Buatlah program C++ dengan class Barang yang memiliki atribut: namaBarang, kodeBarang
+//Program memiliki Constructor berparameter untuk mengisi nilai namaBarang dan kodeBarang
+//Serta memiliki fungsi untuk menampilkan informasi barang
